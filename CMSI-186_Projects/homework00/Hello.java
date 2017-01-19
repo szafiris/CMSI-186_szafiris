75364486be3eba0c960062b36510b96f18896a32
szafiris <szafiris@lion.lmu.edu>
2017-01-19 07:33:52 -0800

    Added comment section

--- CMSI-186_Projects/homework00/Hello.java
+++ CMSI-186_Projects/homework00/Hello.java
@@ -1,3 +1,20 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File Name      :  Greeter.java
+ * Purpose        :  Says "Hello World!" to the user
+ * Author         :  Serena Zafiris
+ * Date           :  2017-01-16
+ * Description    :  See purpose
+ * Notes          :  None
+ * Warnings       :  None
+ * Exceptions     :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ~~~~~~~~~~~~~~~~
+ *          Rev     Date        Modified by:       Reason for change
+ *          ~~~~    ~~~~        ~~~~~~~~~~~~       ~~~~~~~~~~~~~~~~~~~~~~~
+ * @version 1.0.0   2017-01-16  Serena Zafiris     Iniital writing and release
+ * @version 1.0.1   2017-01-17  Serena Zafiris     Adding comment section
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 public class Greeter {
   public static void main( String args[] ) {
     System.out.println( "Hello World!" );
