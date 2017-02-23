8b2606dc7f5ab8cc03b53bc01ff684865396e619
szafiris <szafiris@lion.lmu.edu>
2017-02-23 12:37:28 -0800

    Create HighRoll.java

---
+++ CMSI-186_Projects/homework03/HighRoll.java
@@ -0,0 +1,78 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  HighRoll.java
+ *  Purpose       :  A dice rolling game
+ *  @author       :  Serena Zafiris
+ *  Date          :  2017-02-14
+ *  Description   :  This game allows the user to try and get the highest score possible from rolling multiple die.
+ *                   The user cann roll all the die, roll just one, calculate their score, and see their dice.
+ *                   They can also set the amount of die and the number of sides.
+ *
+ *  Notes         :  Restrictions: no such thing as a "two-sided die" which would be a coin, actually.
+ *                   Also, no such thing as a "three-sided die" which is a physical impossibility without
+ *                   having it be a hollow triangular prism shape, presenting an argument as to whether
+ *                   the inner faces are faces which then should be numbered.  Just start at four for
+ *                   minimum number of faces.
+ *
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the number of sides or pips is out of range
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:      Reason for change/modification
+ *           -----  ----------  ------------      -----------------------------------------------------------
+ *  @version 1.0.0  2017-02-21  Serena Zafiris    Initial writing
+ *  @version 1.0.1  2017-02-22  Serena Zafiris    Fixed methods and character errors
+ *  @version 1.0.2  2017-02-23  Serena Zafiris    Displayed score properly
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class HighRoll {
+  public static void main( String args[] ) {
+    int count = Integer.parseInt( args[0] );
+    int sides = Integer.parseInt( args[1] );
+    int highScore = 0;
+    DiceSet ds = new DiceSet(count, sides);
+    System.out.println( "\n   Welcome to the HighRoll Game\n" +
+    "1. Roll all the dice\n" + "2. Roll a single die\n" + "3. Calculate the score for this set\n" + "4. Save score to highscore\n" +
+    "5. Display the highscore\n");
+    System.out.println( "     Press the 'q' key to quit the program." );
+    BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+      while( true ) {
+        System.out.print( ">>" );
+        String inputLine = null;
+        try {
+          inputLine = input.readLine();
+          if( 0 == inputLine.length() ) {
+             System.out.println("enter some text!:");
+          }
+          System.out.println( "   You typed: " + inputLine );
+          if( 'q' == inputLine.charAt(0) ) {
+            System.out.println("You quit the program. Game over! Final Score: " + highScore);
+             break;
+          }         }
+        catch( IOException ioe ) {
+          System.out.println( "Caught IOException" );
+        }
+        if( inputLine.charAt(0) == '1' ) {
+          ds.roll();
+          System.out.println( ds.toString() );
+         }
+        if( inputLine.charAt(0) == '2' ) {
+          ds.rollIndividual( Character.getNumericValue( inputLine.charAt(2) ) );
+          System.out.println( Character.getNumericValue( inputLine.charAt(2) ) );
+          System.out.println( ds.toString() );
+        }
+        if( inputLine.charAt(0) == '3' ) {
+          System.out.println( "Score for this set : " + ds.sum() );
+        }
+        if( inputLine.charAt(0) == '4' ) {
+          highScore = ds.sum();
+        }
+        if( inputLine.charAt(0) == '5' ) {
+          System.out.println("Your highscore is: " + highScore);
+        }
+      }
+   }
+}
